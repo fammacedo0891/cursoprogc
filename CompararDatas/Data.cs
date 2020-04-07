7fd725bee9b8aa247d062a6241974b376ef36e1c
fammacedo0891 <fam_macedo@hotmail.com>
2020-04-07 15:14:50 -0300

    Prog inicial

---
+++ CompararDatas/Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;                            
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompararDatas
+{
+    class Data
+    {
+
+
+        //public TimeSpan DifDatas { get; set; }
+        //public DateTime DataInicio { get; set; }
+
+        public static DateTime d1;
+        public static DateTime d2;
+
+
+        public static DateTime Data1(DateTime data1)
+        {
+
+            return d1;
+        }
+
+        // definir valor como static permite chamar diretamente no program sem a necessidade de instanciar
+        public static DateTime Data2(DateTime data2)
+        {
+
+            return d2;
+        }
+
+        //public void CalculoData(DateTime dataincio, DateTime datafim)
+        //{
+        //    DifDatas = datafim.Subtract(dataincio);
+        //}
+
+
+
+        public override string ToString()
+        {
+            return "d1: "
+            + d1.ToString("dd/MM/yyyy")
+            + " d2: "
+            + d2.ToString("dd/MM/yyyy");
+        }
+    }
+}
---
+++ CompararDatas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompararDatas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Data Dt = new Data();
+            DateTime pd1, pd2;
+            DateTime pr1, pr2;
+
+            Console.Write("Informe uma data1: ");
+            pd1 = DateTime.Parse(Console.ReadLine());
+
+            Data.d1 = pd1;
+
+            Console.Write("Informe uma data2: ");
+            pd2 = DateTime.Parse(Console.ReadLine());
+
+            Data.d2 = pd2;
+
+            Console.WriteLine("*** trabalhando com atributo diretamente ***");
+
+            Console.Write("Data1: " + Data.d1.ToString("dd/MM/yyyy"));
+            Console.WriteLine();
+            Console.Write("Data2: " + Data.d2.ToString("dd/MM/yyyy"));
+
+
+            Console.WriteLine();
+            Console.WriteLine("*** trabalhando com método diretamente ***");
+
+            pr1 = Data.Data1(pd1);
+            pr2 = Data.Data2(pd2);
+
+            Console.Write("Data1: " + pr1.ToString("dd/MM/yyyy"));
+
+            Console.WriteLine();
+
+            Console.Write("Data2: " + pr2.ToString("dd/MM/yyyy"));
+            Console.WriteLine();
+
+            //Console.Write("Informe a data final: ");
+            //d2 = DateTime.Parse(Console.ReadLine());
+
+            //Console.WriteLine(d1);
+            //CompData = new Data(d1, d2);
+
+            //CompData.CalculoData(d1, d2);
+
+
+        }
+
+
+    }
+}
